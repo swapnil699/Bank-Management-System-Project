0335a2db71dd5b9b53d2f2e14d227eab62e1d7bc
Swapnil Bhosale <bswapnil699@gmail.com>
2024-04-14 19:17:20 +0530

    modified the transaction class

--- src/BankManagementSystem/Transactions.java
+++ src/BankManagementSystem/Transactions.java
@@ -9,7 +9,7 @@ public class Transactions extends JFrame implements ActionListener {
     Transactions(){
         setLayout(null);
 
-        //for image
+        //for image icon
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
         Image i2  = i1.getImage().getScaledInstance(900,900,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
@@ -25,45 +25,45 @@ public class Transactions extends JFrame implements ActionListener {
         image.add(text);              // for text image chya vr sati
 
         // for deposit button ----------------------------------------------------------------------------------
-        deposit = new JButton("Deposit");                          //create a sing in button
-        deposit.setBounds(170,415,150,30);                   //location of sign in button
-        image.add(deposit);                                                           //adding sign in button
+        deposit = new JButton("Deposit");                                 //create a deposit button
+        deposit.setBounds(170,415,150,30);                  // location of deposit button
+        image.add(deposit);                                                    // adding deposit  button
         deposit.addActionListener(this);                                       //when user fit the login button-->it will take action
 
         // for withdrawl button ----------------------------------------------------------------------------------
-        withdrawl = new JButton("Cash Withdrawl");                          //create a sing in button
-        withdrawl.setBounds(355,415,150,30);                   //location of sign in button
-        image.add(withdrawl);                                                           //adding sign in button
-        withdrawl.addActionListener(this);                                       //when user fit the login button-->it will take action
+        withdrawl = new JButton("Cash Withdrawl");                             // create a withdrawl button
+        withdrawl.setBounds(355,415,150,30);                     // location of withdrawl button
+        image.add(withdrawl);                                                       // adding withdrawl button
+        withdrawl.addActionListener(this);                                       // when user fit the login button-->it will take action
 
         // for fastcash button ----------------------------------------------------------------------------------
-        fastcash = new JButton("Fast Cash");                          //create a sing in button
-        fastcash.setBounds(170,450,150,30);                       //location of sign in button
-        image.add(fastcash);                                                           //adding sign in button
-        fastcash.addActionListener(this);                                       //when user fit the login button-->it will take action
+        fastcash = new JButton("Fast Cash");                                    //create a fastcash button
+        fastcash.setBounds(170,450,150,30);                       //location of fastcash button
+        image.add(fastcash);                                                        //adding fastcash button
+        fastcash.addActionListener(this);                                        //when user fit the login button-->it will take action
 
         // for ministatement button ----------------------------------------------------------------------------------
-        ministatement = new JButton("Mini Statement");                          //create a sing in button
-        ministatement.setBounds(355,450,150,30);                       //location of sign in button
-        image.add(ministatement);                                                           //adding sign in button
-        ministatement.addActionListener(this);                                       //when user fit the login button-->it will take action
+        ministatement = new JButton("Mini Statement");                           // create a ministatement button
+        ministatement.setBounds(355,450,150,30);                   // location of ministatement button
+        image.add(ministatement);                                                    // adding ministatement button
+        ministatement.addActionListener(this);                                    // when user fit the login button-->it will take action
 
         // for pinchange button ----------------------------------------------------------------------------------
-        pinchange = new JButton("Pin Change");                          //create a sing in button
-        pinchange.setBounds(170,485,150,30);                       //location of sign in button
-        image.add(pinchange);                                                           //adding sign in button
-        pinchange.addActionListener(this);                                       //when user fit the login button-->it will take action
+        pinchange = new JButton("Pin Change");                                  // create a pinchange button
+        pinchange.setBounds(170,485,150,30);                      // location of pinchange button
+        image.add(pinchange);                                                       // adding pinchange button
+        pinchange.addActionListener(this);                                       // when user fit the login button-->it will take action
 
         // for balanceequiry button ----------------------------------------------------------------------------------
-        balanceequiry = new JButton("Balance Enquiry");                             //create a sing in button
-        balanceequiry.setBounds(355,485,150,30);                      //location of sign in button
-        image.add(balanceequiry);                                                       //adding sign in button
+        balanceequiry = new JButton("Balance Enquiry");                             //create a balanceequiry button
+        balanceequiry.setBounds(355,485,150,30);                      //location of balanceequiry button
+        image.add(balanceequiry);                                                       //adding balanceequiry button
         balanceequiry.addActionListener(this);                                       //when user fit the login button-->it will take action
 
         // for exit button ----------------------------------------------------------------------------------
-        exit = new JButton("Exit");                                         //create a sing in button
-        exit.setBounds(355,520,150,30);                       //location of sign in button
-        image.add(exit);                                                        //adding sign in button
+        exit = new JButton("Exit");                                         //create a exit button
+        exit.setBounds(355,520,150,30);                       //location of exit button
+        image.add(exit);                                                        //adding exit button
         exit.addActionListener(this);                            //when user fit the exit button-->it will take action -> close windown
 
         //color the frame  -------------------------------------------------------------------------------------
